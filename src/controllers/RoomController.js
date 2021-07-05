a0681d3ffb386beac001dac13f4d51970664cce9
Jonathan Souza <68517888+DVJonathanSouza@users.noreply.github.com>
2021-07-05 00:42:18 -0300

    Add files via upload

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -8,20 +8,60 @@ module.exports = {
         const pass = req.body.password
         let roomId
 
-        // Gerar aleatoriamente os "roomId"
-        for (var i = 0; i < 6; i++) {
-            i == 0 ? roomId = Math.floor(Math.random() * 10).toString() : roomId += Math.floor(Math.random() * 10).toString()
+        let isRoom = true
+        while (isRoom) {
+            // Gerar aleatoriamente os "roomId" da sala
+            for (var i = 0; i < 6; i++) {
+                i == 0 ? roomId = Math.floor(Math.random() * 10).toString() : roomId += Math.floor(Math.random() * 10).toString()
+            }
+
+            // Pegando todos "all" os id da table rooms
+            const roomsExistIds = await db.all(`SELECT id FROM rooms`)
+
+            // Verificando se os "id" existentes ja existe
+            isRoom = roomsExistIds.some(roomsExistIds => roomsExistIds === roomId)
+
+            // Se for "false", e nao existir nenhum "id" igual ao criado
+            if (!isRoom) {
+                // Ai, insere a table rooms no banco com suas colunas "id" e "pass"
+                await db.run(`INSERT INTO rooms (
+                        id,
+                        pass
+                    ) VALUES (
+                        ${parseInt(roomId)},
+                        ${pass}
+                    )`)
+            }
         }
 
-        await db.run(`INSERT INTO rooms (
-            id,
-            pass
-        ) VALUES (
-            ${parseInt(roomId)},
-            ${pass}
-        )`)
-
+        // Fecha a gaveta do banco de dados
         await db.close()
+
+        res.redirect(`/room/${roomId}`)
+    },
+
+    async open(req, res) {
+        const db = await Database()
+
+        const roomId = req.params.room
+
+        const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 0`)
+
+        const questionsRead = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 1`)
+
+        let isNoQuestions
+
+        if (questions.length == 0) {
+            if (questionsRead.length == 0) {
+                isNoQuestions = true
+            }
+        }
+
+        res.render("room", { roomId: roomId, questions: questions, questionsRead: questionsRead, isNoQuestions: isNoQuestions })
+    },
+
+    enter(req, res) {
+        const roomId = req.body.roomId
 
         res.redirect(`/room/${roomId}`)
     }
--- src/route.js
+++ src/route.js
@@ -10,19 +10,26 @@ const route = express.Router();
 
 // Indicando nossa rota do "index", e informando que deve ser renderizado nesta URL a page "enter-room"
 route.get('/', (req, res) => res.render("index", { page: 'enter-room' }));
+
 // Indicando nossa rota do "index", e informando com uma variavel que deve ser renderizado nesta URL a page "create-pass"
 route.get('/create-pass', (req, res) => res.render("index", { page: 'create-pass' }));
+
+// Estamos enviando indiretamente req e res para a "RoomController.index"
+route.post('/create-room', RoomController.create);
+
 // Indicando nossa rota do "room"
 // Lebrando que o ":" no comeco eh para informar para o "express" que nao sabemos o que vem na frente
 // No lugar do ":room" sera o ID da sala que criamos
-route.get('/room/:room', (req, res) => res.render("room"));
+route.get('/room/:room', RoomController.open);
+
+route.post('/enterroom', RoomController.enter);
+
+route.post('/question/create/:room', QuestionController.create);
 
 // ----Formato que o formulario de dentro da modal tem que passar a informacao
 // Lebrando que o ":" no comeco eh para informar para o "express" que nao sabemos o que vem na frente
 // Estamos enviando indiretamente req e res para a "QuestionController.index"
 route.post('/question/:room/:question/:action', QuestionController.index);
-// Estamos enviando indiretamente req e res para a "RoomController.index"
-route.post('/create-room', RoomController.create);
 
 // Para poder importando o arquivo "route.js"
 module.exports = route
